10980d4b92c12f1f7d1023154f99e54aaeaace8e
Đỗ Xuân Thanh <thanhcr2001@gmail.com>
2022-01-16 15:42:43 +0700

    Update Paging, Review and product

--- components/products/productsController.js
+++ components/products/productsController.js
@@ -1,10 +1,25 @@
-//const productsService = require('./productsService');
+var express = require("express");
+const async = require("hbs/lib/async");
+var router = express.Router();
+var products = require('../../models/products')
 
-exports.list = (req,res) => {
-    //const product = await productsService.list();
-    res.render('products/productsList');
-}
+const productService=require('./productsService');
+const productCategories=require('../../models/productcategories')
 
+exports.list = async (req, res)=>{
+	const productPerPage=6;
+	const pageNumber=!isNaN(req.query.page) &&req.query.page>1?req.query.page:1;
+	const products=await productService.list(pageNumber,productPerPage);
+	res.render('products/productsList', {products});
+};
+
+// exports.listProduct = async (req, res)=>{
+// 	let type="TY001";
+// 	const productPerPage=6;
+// 	const pageNumber=!isNaN(req.query.page) &&req.query.page>1?req.query.page:1;
+// 	const products=await productService.listProduct(type,pageNumber,productPerPage);
+// 	res.render('product/soapList', {products});
+// };
 exports.edit = (req,res) => {
 
     res.render('products/productsEdit');
--- components/products/productsService.js
+++ components/products/productsService.js
@@ -1,5 +1,19 @@
-/*const models = require('../../models');
+const products = require('../../models/products');
 
-exports.list = () => {
-    return models.products.findAll();
-}*/
+exports.list = (pageNumber, productPerPage) => {
+    return products
+        .find().lean()
+        .sort({ProductID: 1})
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * productPerPage) : 0)
+        .limit(productPerPage);
+};
+exports.listProduct = (type, pageNumber, productPerPage) => {
+    return products
+        .find({Category: type}).lean()
+        .skip(pageNumber > 0 ? ((pageNumber - 1) * productPerPage) : 0)
+        .limit(productPerPage);
+};
+
+exports.listDetail = (id) => {
+    return products.find({ProductID: id});
+};
